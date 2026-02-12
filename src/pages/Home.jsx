5cd1498978db0a99b019e980555acba2c0254793
jayeshshendurnikar <shendurnikarjayesh2001@gmail.com>
2026-02-12 10:47:38 +0530

    home page added

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import ProductList from "./ProductList";
+
+function Home() {
+  return (
+    <section>
+      <h2 className="page-title">Products</h2>
+      <ProductList />
+    </section>
+  );
+}
+
+export default Home;
