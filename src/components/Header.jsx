49c888196e7f8aabf29fc11ce82b619d908bfa3e
jayeshshendurnikar <shendurnikarjayesh2001@gmail.com>
2026-02-12 12:55:11 +0530

    improved css in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,14 +14,12 @@ function Header() {
   };
 
   return (
-    <header className="bg-emerald-400 shadow-md">
+    <header className="bg-blue-500 shadow-md">
       <div className="max-w-7xl mx-auto px-4 py-4">
-        
         {/* Top Section */}
         <div className="flex items-center justify-between">
-          
           {/* Logo */}
-          <h1 className="text-2xl font-bold text-white">
+          <h1 className="text-2xl font-bold text-black">
             <Link to="/">MyShop</Link>
           </h1>
 
@@ -30,20 +28,27 @@ function Header() {
             type="text"
             onChange={handleSearch}
             placeholder="Search products..."
-            className="hidden md:block border rounded-full px-4 py-2 w-1/3 text-center focus:outline-none"
+            className="hidden md:block border border-white rounded-full px-4 py-2 w-1/3 text-center bg-blue-300
+             focus:outline-none focus:ring-2 focus:ring-white focus:bg-white
+             transition duration-200"
           />
 
           {/* Desktop Menu */}
           <ul className="hidden md:flex items-center gap-6 font-medium text-white">
             <li className="hover:text-black transition">
-              <Link to="/">Home</Link>
+              <Link className="relative
+                after:absolute after:left-0 after:-bottom-1
+                after:h-[2px] after:w-0 after:bg-black
+                after:transition-all after:duration-300
+                hover:after:w-full hover:text-black" to="/">Home</Link>
             </li>
 
             <li className="relative hover:text-black transition">
-              <Link to="/cart">
-                Cart
-                ({cartCount })
-              </Link>
+              <Link className="relative
+                after:absolute after:left-0 after:-bottom-1
+                after:h-[2px] after:w-0 after:bg-black
+                after:transition-all after:duration-300
+                hover:after:w-full hover:text-black" to="/cart">Cart ({cartCount})</Link>
             </li>
           </ul>
 
@@ -59,7 +64,6 @@ function Header() {
         {/* Mobile Menu */}
         {menu && (
           <div className="flex flex-col gap-4 mt-4 md:hidden items-center font-medium text-white">
-            
             <input
               type="text"
               onChange={handleSearch}
@@ -76,7 +80,6 @@ function Header() {
             </Link>
           </div>
         )}
-
       </div>
     </header>
   );
