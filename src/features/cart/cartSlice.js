7d03f1289cf94c6f1261c789b86c474eddc66b3a
jayeshshendurnikar <shendurnikarjayesh2001@gmail.com>
2026-02-12 10:17:11 +0530

    decrease quantity and clear cart implementedin redux store

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -42,6 +42,19 @@ const cartSlice = createSlice({
         item.quantity += 1;
       }
     },
+    decreaseQty: (state, action) => {
+      const item = state.items.find(
+        (item) => item.id === action.payload
+      );
+      if (item && item.quantity > 1) {
+        item.quantity -= 1;
+      }
+    },
+
+    // Clear cart after order placed
+    clearCart: (state) => {
+      state.items = [];
+    },
 
   },
 });
@@ -50,6 +63,8 @@ export const {
   addToCart,
   removeFromCart,
   increaseQty,
+  decreaseQty,
+  clearCart,
 } = cartSlice.actions;
 
 export default cartSlice.reducer;
